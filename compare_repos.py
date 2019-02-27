5f71e5246cc36e140c54900d09cea2f7b9abf91f
alex <alexanderhfsteele@gmail.com>
2019-02-27 10:21:49 +0000

    1 vs. many framework comparison

--- compare_repos.py
+++ compare_repos.py
@@ -1,3 +1,4 @@
+import os
 from git_clone import build_org_repos
 from identify_framework import get_languages, get_frameworks
 from identify_endpoints import get_flask_endpoints
@@ -14,89 +15,92 @@ print(" ")
 print(" -------------------------- ")
 print(" ")
 
-repo1 = input("Enter a repo to evaluate: ")
-repo2 = input("Enter another repo to evaluate and compare: ")
 
+repo = input("Enter a repo to evaluate: ")
 print(" ")
 
-
-
-#main repo
+# main repo
 print(" ")
-print(" --------- " + repo1 + " ----------- ")
+print(" --------- " + repo + " ----------- ")
 print(" ")
 
 print("Language(s):  ", end=" ")
-for language in get_languages(repo1):
+for language in get_languages(repo):
     if language:
         print(language, end=" ")
 print(" ")
 
-frameworks = get_frameworks(repo1)
+frameworks = get_frameworks(repo)
 
 print("Framework(s): ", end=" ")
 for framework in frameworks:
     if framework:
         print(framework, end=" ")
-        
-print(" ")
 
 if "Flask" in frameworks:
     print("")
     print("Identifying Flask Endpoint(s):")
 
-    for endpoints in get_flask_endpoints(repo1):
-        print(endpoints)
-
-print(" ")
-
-
-
-# other repo
-print(" ")
-print(" --------- " + repo2 + " ----------- ")
-print(" ")
-
-print("Language(s):  ", end=" ")
-for language in get_languages(repo2):
-    if language:
-        print(language, end=" ")
-print(" ")
-
-other_frameworks = get_frameworks(repo2)
-
-print("Framework(s): ", end=" ")
-for framework in other_frameworks:
-    if framework:
-        print(framework, end=" ")
-
-if "Flask" in other_frameworks:
-    print("")
-    print("Identifying Flask Endpoint(s):")
-
-    for endpoints in get_flask_endpoints(repo2):
+    for endpoints in get_flask_endpoints(repo):
         print(endpoints)
         
 print(" ")
 
-print(" ")
-print(" -------------------------- ")
-print(" ")
-
-
-# compare
-
-frameworks_intersect = intersect(frameworks, other_frameworks)
-
-print("Matching frameworks: ", end=" ")
-for match in frameworks_intersect:
-    if match:
-        print(match, end=" ")
-print(" ")
+contine = input("Press enter to continue")
 
 print(" ")
 print(" -------------------------- ")
 print(" ")
+
+
+# other repo
+other_repos = os.listdir('./cloned_repos/')
+for other_repo in other_repos:
+    print(" ")
+    print(" --------- " + other_repo + " ----------- ")
+    print(" ")
+    
+    print("Language(s):  ", end=" ")
+    for language in get_languages(other_repo):
+        if language:
+            print(language, end=" ")
+    print(" ")
+
+    other_frameworks = get_frameworks(other_repo)
+
+    print("Framework(s): ", end=" ")
+    for framework in other_frameworks:
+        if framework:
+            print(framework, end=" ")
+
+    print(" ")
+
+    if "Flask" in other_frameworks:
+        print("")
+        print("Identifying Flask Endpoint(s):")
+
+        for endpoints in get_flask_endpoints(other_repo):
+            print(endpoints)
+        print(" ")
+
+    frameworks_intersect = intersect(frameworks, other_frameworks)
+
+    print("Matching frameworks: ", end=" ")
+    for match in frameworks_intersect:
+        if match:
+            print(match, end=" ")
+    print(" ")
+
+    print(" ")
+    print(" -------------------------- ")
+    print(" ")
+
+
+
+
+
+
+
 
 
 
