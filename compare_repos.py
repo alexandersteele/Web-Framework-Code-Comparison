d9469f710840075f12ab2268399bf7784ee5b65c
alex <alexanderhfsteele@gmail.com>
2019-02-28 10:27:24 +0000

    Added many vs. many comparison and basic threshold limiting

--- compare_repos.py
+++ compare_repos.py
@@ -16,111 +16,52 @@ print(" -------------------------- ")
 print(" ")
 
 
-repo = input("Enter a repo to evaluate: ")
-print(" ")
+repos = os.listdir('./cloned_repos/')
+for repo in repos:
+    frameworks = get_frameworks(repo)
+    endpoints = get_flask_endpoints(repo)
 
-# main repo
-print(" ")
-print(" --------- " + repo + " ----------- ")
-print(" ")
+    # other repo
+    other_repos = os.listdir('./cloned_repos/')
+    for other_repo in other_repos:
 
-print("Language(s):  ", end=" ")
-for language in get_languages(repo):
-    if language:
-        print(language, end=" ")
-print(" ")
+        if other_repo != repo:
 
-frameworks = get_frameworks(repo)
-
-print("Framework(s): ", end=" ")
-for framework in frameworks:
-    if framework:
-        print(framework, end=" ")
-
-endpoints = get_flask_endpoints(repo)
-
-if "Flask" in frameworks:
-    print("")
-    print("Identifying Flask Endpoint(s):")
-
-    for endpoint in endpoints:
-        print(endpoint)
-        
-print(" ")
-
-contine = input("Press enter to continue")
-
-print(" ")
-print(" -------------------------- ")
-print(" ")
+            other_frameworks = get_frameworks(other_repo)
+            other_endpoints = get_flask_endpoints(other_repo)
 
 
-# other repo
-other_repos = os.listdir('./cloned_repos/')
-for other_repo in other_repos:
-
-    if other_repo != repo:
-        
-        print(" ")
-        print(" --------- " + other_repo + " ----------- ")
-        print(" ")
-        
-        print("Language(s):  ", end=" ")
-        for language in get_languages(other_repo):
-            if language:
-                print(language, end=" ")
-        print(" ")
-
-        other_frameworks = get_frameworks(other_repo)
-
-        print("Framework(s): ", end=" ")
-        for framework in other_frameworks:
-            if framework:
-                print(framework, end=" ")
-
-        print(" ")
-
-        other_endpoints = get_flask_endpoints(other_repo)
-
-        if "Flask" in other_frameworks:
-            print("")
-            print("Identifying Flask Endpoint(s):")
-
-            for endpoint in other_endpoints:
-                print(endpoint)
-            print(" ")
-
-        frameworks_intersect = intersect(frameworks, other_frameworks)
-        endpoints_intersect = intersect(endpoints, other_endpoints)
-        endpoints_percent = 0
-
-        print("Matching frameworks with " + repo + ": ", end=" ")
-        for match in frameworks_intersect:
-            if match:
-                print(match, end=" ")
-        print(" ")
-
-        if len(endpoints_intersect) > 0:
+            frameworks_intersect = intersect(frameworks, other_frameworks)
+            endpoints_intersect = intersect(endpoints, other_endpoints)
+            
+            if len(endpoints_intersect) > 0:
                 endpoints_percent = (len(endpoints_intersect) / len(other_endpoints)) * 100
-        print("Matching endpoints with " + repo + ": " + str(endpoints_percent) + "%", end=" ")
-        print(" ")
-        for match in endpoints_intersect:
-            if match:
-                print(match, end=" ")
+            else:
+                endpoints_percent = 0
+                    
+
+            if len(frameworks_intersect) > 1 and endpoints_percent > 0:
+
                 print(" ")
-        print(" ")
+                print(" --------- " + repo + " compared to " + other_repo + " ----------- ")
+                print(" ")
 
-        print(" ")
-        print(" -------------------------- ")
-        print(" ")
+                print("Matching frameworks : ", end=" ")
+                for match in frameworks_intersect:
+                    if match:
+                        print(match, end=" ")
+                print(" ")
 
+                
+                print("Matching endpoints with: " + str(endpoints_percent) + "%", end=" ")
+                print(" ")
+                for match in endpoints_intersect:
+                    if match:
+                        print(match, end=" ")
+                        print(" ")
+                print(" ")
 
-
-
-
-
-
-
-
-
+                print(" ")
+                print(" -------------------------- ")
+                print(" ")
 
